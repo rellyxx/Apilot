a470df8afdf2e6e78e61c0751fc1fb55e96e7909
relly <rellyxx@gmail.com>
2024-04-12 23:20:34 +0800

    2

--- Apilot.py
+++ Apilot.py
@@ -51,7 +51,7 @@ class Apilot(Plugin):
         logger.debug("[Apilot] on_handle_context. content: %s" % content)
 
         # 走茉莉云
-        if "黑丝" in content or "白丝" in content or "美女" in content or "点歌" in content or "猜拳" in content or "签到" in content or "抽签" in content or "倒计时" in content:
+        if "奶子" in content or "黑丝" in content or "白丝" in content or "美女" in content or "点歌" in content or "猜拳" in content or "签到" in content or "抽签" in content or "倒计时" in content:
             e_context.action = EventAction.BREAK_PASS  # 事件结束，并跳过处理context的默认逻辑
             return
         
